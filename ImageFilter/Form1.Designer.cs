ac45f3af9158565af1c1868ef6df11fd23df1fb6
Amin Hasanov <44285676+aminhesenov@users.noreply.github.com>
2024-12-09 11:30:51 +0400

    Color Filter

---
+++ ImageFilter/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace ImageFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gray;
+            panel1.Location = new Point(-2, 47);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(805, 10);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Gray;
+            panel2.Location = new Point(563, 49);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(10, 370);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Gray;
+            panel3.Location = new Point(568, 107);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(235, 10);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Gray;
+            panel4.Location = new Point(568, 167);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(235, 10);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Gray;
+            panel5.Location = new Point(570, 227);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(235, 10);
+            panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.Gray;
+            panel6.Location = new Point(570, 287);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(253, 10);
+            panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.Gray;
+            panel7.Location = new Point(570, 347);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(268, 10);
+            panel7.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.Gray;
+            panel8.Location = new Point(571, 407);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(234, 11);
+            panel8.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(85, 77);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(383, 304);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(94, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(182, 43);
+            button1.TabIndex = 9;
+            button1.Text = "Select image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(620, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 20);
+            label1.TabIndex = 10;
+            label1.Text = "Color Filters";
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(579, 63);
+            button2.Name = "button2";
+            button2.Size = new Size(209, 38);
+            button2.TabIndex = 11;
+            button2.Text = "Demonic";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(579, 123);
+            button3.Name = "button3";
+            button3.Size = new Size(209, 38);
+            button3.TabIndex = 12;
+            button3.Text = "Dramatic";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(579, 183);
+            button4.Name = "button4";
+            button4.Size = new Size(209, 38);
+            button4.TabIndex = 13;
+            button4.Text = "Freezing";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(579, 243);
+            button5.Name = "button5";
+            button5.Size = new Size(209, 38);
+            button5.TabIndex = 14;
+            button5.Text = "Holy";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button6.ForeColor = Color.White;
+            button6.Location = new Point(579, 303);
+            button6.Name = "button6";
+            button6.Size = new Size(209, 38);
+            button6.TabIndex = 15;
+            button6.Text = "Natural";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button7.ForeColor = Color.White;
+            button7.Location = new Point(579, 363);
+            button7.Name = "button7";
+            button7.Size = new Size(209, 38);
+            button7.TabIndex = 16;
+            button7.Text = "Sunny";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Green;
+            button8.FlatStyle = FlatStyle.Popup;
+            button8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button8.ForeColor = Color.White;
+            button8.Location = new Point(85, 396);
+            button8.Name = "button8";
+            button8.Size = new Size(149, 32);
+            button8.TabIndex = 17;
+            button8.Text = "Save image";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(192, 0, 0);
+            button9.FlatStyle = FlatStyle.Popup;
+            button9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            button9.ForeColor = Color.White;
+            button9.Location = new Point(319, 396);
+            button9.Name = "button9";
+            button9.Size = new Size(149, 32);
+            button9.TabIndex = 18;
+            button9.Text = "Exit";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(88, 43);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 19;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Navy;
+            ClientSize = new Size(800, 441);
+            Controls.Add(pictureBox2);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(panel6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel8);
+            Controls.Add(panel7);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private PictureBox pictureBox2;
+    }
+}
